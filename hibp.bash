b8cb43fac8a8877e62c76cce712cb3b8d3128b4f
manila <manila@users.noreply.github.com>
2018-10-24 18:09:32 -0600

    Check pass file existance eloquently

--- hibp.bash
+++ hibp.bash
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-VERSION="0.1.0"
-
-get_password() {
-	$GPG -d "${GPG_OPTS[@]}" "$PREFIX/$1.gpg" | head -n 1
-}
+VERSION="0.1.1"
 
 get_pwned_password_list() {
 	curl -s https://api.pwnedpasswords.com/range/$1 | awk -F ':' '{ print tolower($1) }'	
@@ -14,29 +10,32 @@ get_sha1() {
 	echo -n $1 | openssl sha1 | awk '{ print tolower($2) }'
 }
 
-cmp_hashes() {
-	echo "hi"
-}
-
 cmd_check_pwnage() {
-	local_hash="$( get_sha1 $1 )"
-	found_msg="Not found in Pwned Passwords Database"
-
-	echo "$(get_pwned_password_list ${local_hash:0:5})" > remote_hashes_list
-
-	readarray remote_hashes < remote_hashes_list
-
-	for remote_hash in "${remote_hashes[@]}"; do
-		if [ "${remote_hash:0:5}" == "${local_hash:5:5}" ]; then
-			found_msg="Found in Pwned Passwords Database"
-		fi
-	done
-
-	echo $found_msg
+	if [[ -z "$1" ]]; then
+		msg="Please enter the name of a password file to check"
+		die "$msg"
+	elif [[ "$(cmd_show $1)" ]]; then
+		pass="$(cmd_show $1)"
+		local pass_hash="$( get_sha1 $pass )"
+		local msg="Not found in Pwned Passwords Database"
+	
+		echo "$(get_pwned_password_list ${pass_hash:0:5})" > remote_hashes_list
+	
+		readarray remote_hashes < remote_hashes_list
+	
+		for remote_hash in "${remote_hashes[@]}"; do
+			if [ "${remote_hash:0:5}" == "${local_hash:5:5}" ]; then
+				msg="Found in Pwned Passwords Database"
+			fi
+		done
+	
+		echo $msg
+	fi
 }
+
 
 case "$1" in
-	*) cmd_check_pwnage "$(get_password $@)";;
+	*) cmd_check_pwnage $@;;
 esac
 
 exit 0

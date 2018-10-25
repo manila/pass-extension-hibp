f6bb06ff20213f2fdddb567105c22b9e15eb1ef0
manila <manila@users.noreply.github.com>
2018-10-24 21:45:38 -0600

    Added mktemp to store hashes and consent dialogue

--- hibp.bash
+++ hibp.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 VERSION="0.1.1"
+AWK=awk
 
 get_pwned_password_list() {
-	curl -s https://api.pwnedpasswords.com/range/$1 | awk -F ':' '{ print tolower($1) }'	
+	curl -s https://api.pwnedpasswords.com/range/$1 | $AWK -F ':' '{ print tolower($1) }'	
 }
 
 get_sha1() {
@@ -12,16 +13,19 @@ get_sha1() {
 
 cmd_check_pwnage() {
 	if [[ -z "$1" ]]; then
-		msg="Please enter the name of a password file to check"
+		msg="Error: No passfile specified."
 		die "$msg"
 	elif [[ "$(cmd_show $1)" ]]; then
 		pass="$(cmd_show $1)"
 		local pass_hash="$( get_sha1 $pass )"
 		local msg="Not found in Pwned Passwords Database"
+
+		tmpdir 
+		local tmpfile="$( mktemp $SECURE_TMPDIR/XXXXXX )"
 	
-		echo "$(get_pwned_password_list ${pass_hash:0:5})" > remote_hashes_list
+		echo "$(get_pwned_password_list ${pass_hash:0:5})" > $tmpfile
 	
-		readarray remote_hashes < remote_hashes_list
+		readarray remote_hashes < $tmpfile
 	
 		for remote_hash in "${remote_hashes[@]}"; do
 			if [ "${remote_hash:0:5}" == "${pass_hash:5:5}" ]; then
@@ -29,10 +33,12 @@ cmd_check_pwnage() {
 			fi
 		done
 	
+		rm -f remote_hashes_list
 		echo $msg
 	fi
 }
 
+yesno "Warning: This extensions sends your passwords to a third party API, if you agree to this and understand the potential risks enter Y"
 
 case "$1" in
 	*) cmd_check_pwnage $@;;
